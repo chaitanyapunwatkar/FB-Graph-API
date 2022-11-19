d4ce3bdcbf067649341485d9f011a7e1585d566b
chaitanyapunwatkar <chaitanya.punwatkar@gmail.com>
2022-11-20 04:50:43 +0530

    proper comments added

--- Main.py
+++ Main.py
@@ -4,14 +4,14 @@ import requests
 import json
 import datetime
 import pandas as pd
-from FBData_API import *
+from FBData_API import *  
 
 app = Flask(__name__)
 
  # Home page content
 @app.route("/", methods=("POST", "GET"))
 def home(): 
-    df = extract_data() # fetchs data from facebook graph API.
+    df = extract_data() # fetchs insghts data in dataframes format from facebook graph API.
     return render_template('index.html',  column_names=df.columns.values, row_data=list(df.values.tolist()),
                            link_column="Media_URL", zip=zip) # Displays in html format in Web
     
